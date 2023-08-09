fcf6c344afd4c861ee2246c7edb241e77b982011
Pepijn van Wijk <pvwpvw@protonmail.com>
2023-08-09 11:43:54 +0300

    More work

--- uvaGPT.py
+++ uvaGPT.py
@@ -9,8 +9,35 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 import os
 import pickle
-import textract
+from pdfminer.high_level import extract_text
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast
+
+
+def get_all_models():
+    return [m.removesuffix(".pdf") for m in os. listdir("PDFs/") if m != ".DS_Store"]
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model',
+                        type=str,
+                        default="arco", # terug naar all doen wanneer geimplementeerd
+                        choices= ALL_MODELS,
+                        help=f'The model (course) you want to query. \
+                        Model must be one of these values: {ALL_MODELS}, and defaults \
+                        to all models.')
+
+    model = parser.parse_args().model
+    if model not in ALL_MODELS:
+        print(f"Model: {model} not supported. please pick one of the following models: {ALL_MODELS}")
+        exit()
+
+    return model
+
+
+def combine_dbs():
+
+    return
 
 
 class UvaGPT:
@@ -22,17 +49,22 @@ class UvaGPT:
         self.db_name = f"DBs/{model}.pkl"
         self.chunks_path = f"chunks/{model}_chunks.index"
 
-        if not os.path.isfile(self.txt_file):
+        # TODO: Automatically create 'all' database.
+        if not os.path.isfile(self.txt_file) and self.model != "all":
             print(f"Creating {self.txt_file}...")
             self.parse_pdf()
-        if not os.path.isfile(self.db_name):
+        if not os.path.isfile(self.db_name) and self.model != "all":
             print(f"Splitting {self.txt_file}...")
             chunks = self.split_txt()
             print(f"Creating {self.db_name}...")
             self.create_faiss_db(chunks)
 
+        if self.model == "all" and not os.path.isfile(self.db_name):
+            combine_dbs()
+
         self.load_faiss_db()
         memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+        # Maybe 3.5 turbo gebruiken ipv davinci?
         self.qa_chain = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0.05), self.db.as_retriever(), memory=memory)
 
 
@@ -50,9 +82,9 @@ class UvaGPT:
 
 
     def parse_pdf(self):
-        text = textract.process(self.pdf_file)
+        text = extract_text(self.pdf_file)
         with open(self.txt_file, 'w+') as f:
-            f.write(text.decode('utf-8'))
+            f.write(text)
 
 
     def split_txt(self):
@@ -90,19 +122,7 @@ class UvaGPT:
         return result["answer"]
 
 
-all_models = ['all', 'arco']
-
-parser = argparse.ArgumentParser()
-# TODO: Automatically create 'all' database.
-parser.add_argument('-m', '--model', type=str, default="all",
-                    help=f'The model (course) you want to query. \
-                    Model must be one of these values: {all_models}, and defaults \
-                    to all models.')
-
-model = parser.parse_args().model
-if model not in all_models:
-    print(f"Model: {model} not supported. please pick one of the following models: {all_models}")
-    exit()
-
+ALL_MODELS = get_all_models()
+model = parse()
 uva = UvaGPT(model=model)
 uva.run()
