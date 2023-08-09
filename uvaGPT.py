f1d5ec5114a6ef11a7fd57e3ad2f6aeebc74de20
Pepijn van Wijk <pvwpvw@protonmail.com>
2023-08-09 13:48:31 +0300

    Added API key check

--- uvaGPT.py
+++ uvaGPT.py
@@ -21,11 +21,10 @@ def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--model',
                         type=str,
-                        default="arco", # terug naar all doen wanneer geimplementeerd
+                        required=True,
                         choices= ALL_MODELS,
                         help=f'The model (course) you want to query. \
-                        Model must be one of these values: {ALL_MODELS}, and defaults \
-                        to all models.')
+                        Model must be one of these values: {ALL_MODELS}.')
 
     model = parser.parse_args().model
     if model not in ALL_MODELS:
@@ -35,32 +34,27 @@ def parse():
     return model
 
 
-def combine_dbs():
-
-    return
-
-
 class UvaGPT:
-    def __init__(self, model="all"):
+    def __init__(self, model="arco"):
         load_dotenv()
+        if os.environ.get('OPENAI_API_KEY') is None:
+            print("Please provide a valid OpenAI API key in the .env file. See .env.example for more information")
+            exit(1)
+
         self.model = model
         self.pdf_file = f"PDFs/{model}.pdf"
         self.txt_file = f"txts/{model}.txt"
         self.db_name = f"DBs/{model}.pkl"
         self.chunks_path = f"chunks/{model}_chunks.index"
 
-        # TODO: Automatically create 'all' database.
-        if not os.path.isfile(self.txt_file) and self.model != "all":
+        if not os.path.isfile(self.txt_file):
             print(f"Creating {self.txt_file}...")
             self.parse_pdf()
-        if not os.path.isfile(self.db_name) and self.model != "all":
+        if not os.path.isfile(self.db_name):
             print(f"Splitting {self.txt_file}...")
             chunks = self.split_txt()
             print(f"Creating {self.db_name}...")
             self.create_faiss_db(chunks)
-
-        if self.model == "all" and not os.path.isfile(self.db_name):
-            combine_dbs()
 
         self.load_faiss_db()
         memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
@@ -71,7 +65,7 @@ class UvaGPT:
     def run(self):
         print("Starting chat. Type 'q' or 'exit' to quit.")
         while True:
-            query = input(f"Model: {model}GPT. Ask your question: ")
+            query = input(f"Model: {self.model}GPT. Ask your question: ")
             if not query:
                 continue
             if query == "q" or query == "exit":
@@ -91,7 +85,7 @@ class UvaGPT:
         with open(self.txt_file, "r") as f:
             text = f.read()
 
-        text_splitter = CharacterTextSplitter(chunk_size=1250, separator="\n")
+        text_splitter = CharacterTextSplitter(chunk_size=1250, separator="\n\n\n")
         chunks = []
         splits = text_splitter.split_text(text)
         chunks.extend(splits)
