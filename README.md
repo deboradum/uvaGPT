30bdadce6419087e402f1d22ac41e923ef1312e7
Pepijn van Wijk <pvwpvw@protonmail.com>
2023-08-18 14:05:26 +0400

    Updated to support local inference

--- README.md
+++ README.md
@@ -19,7 +19,24 @@ A list of available model names can be found by running
 ```
 python3 uvaGPT.py -h
 ```
+Other optional parameters are:
+```
+-h, --help            show this help message and exit
+-m --model            The model you want to query.
+--local               Run a local embedder and chat model in stead of the OpenAI api.
+--save                Save chat history, including the source of the answer, to a local json file.
+```
 If you want to add a custom model, simply place the pdf- or txt file in its respective directory and run the program. It will automatically create a Faiss database that is allows lookup of questions.
+
+
+# Local inference
+Besides using OpenAI's Davinci-003 model, local inference is also supported. For local inference to work, you'll need [llama.cpp](https://github.com/ggerganov/llama.cpp) installed and you will need valid GGML model weights placed in the `models/` directory. These models can be downloaded [here (7B)](https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/tree/main), [here (13B)](https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/tree/main) or [here (70B)](https://huggingface.co/TheBloke/Llama-2-70B-Chat-GGML/tree/main). You can also [request access](https://ai.meta.com/resources/models-and-libraries/llama-downloads/) and convert the weights to GGML yourself. Keep in mind that the llama chat model weights are needed, not the regular weights.
+
+# Similar projects
+I made some other projects based on this project. These other projects do mostly the same thing, but the documents are static and focussed on a specific niche.
+- [pdfGPT](https://github.com/deboradum/pdfGPT), chat with any pdf to your choosing.
+- [pastorGPT](https://github.com/deboradum/PastorGPT), chat with the Bible.
+- [ImamGPT](https://github.com/deboradum/ImamGPT), chat with the Quran.
 
 # Demo
 .
--- uvaGPT.py
+++ uvaGPT.py
@@ -1,15 +1,19 @@
 import argparse
 from dotenv import load_dotenv
 import faiss
+import json
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain import OpenAI
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
 from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import LlamaCpp
 from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
 import os
 import pickle
 from pdfminer.high_level import extract_text
+import torch
 
 
 # Returns a list of all available models.
@@ -26,23 +30,36 @@ def parse_args():
                         choices= ALL_MODELS,
                         help=f'The model (course) you want to query. \
                         Model must be one of these values: {ALL_MODELS}.')
+    parser.add_argument("--local",
+                        action="store_true",
+                        help="Run a local embedder and chat model in stead of the OpenAI api.")
+    parser.add_argument("--save",
+                        action="store_true",
+                        help="Save chat history, including the source of the answer to a local json file.")
 
-    return parser.parse_args().model
+    model = parser.parse_args().model
+    local = parser.parse_args().local
+    save = parser.parse_args().save
+    return model, local, save
 
 
 class UvaGPT:
-    def __init__(self, model="arco"):
+    def __init__(self, local, save_convo, model="arco"):
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
         load_dotenv()
         # Checks for openAI API key.
-        if os.environ.get('OPENAI_API_KEY') is None:
-            print("Please provide a valid OpenAI API key in the .env file. See .env.example for more information")
-            exit(1)
+        if os.environ.get('OPENAI_API_KEY') is None and not local:
+            print("Please provide a valid OpenAI api key in the .env file. See .env.example for more information")
+            raise Exception("No OpenAI api key")
 
         self.model = model
         self.pdf_file = f"PDFs/{model}.pdf"
         self.txt_file = f"txts/{model}.txt"
         self.db_name = f"DBs/{model}.pkl"
         self.chunks_path = f"chunks/{model}_chunks.index"
+        self.local = local
+        self.save_convo = save_convo
+        self.save_path = None
 
         if not os.path.isfile(self.txt_file):
             print(f"Creating {self.txt_file}...")
@@ -54,21 +71,88 @@ class UvaGPT:
             self.create_faiss_db(chunks)
 
         self.load_faiss_db()
-        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-        self.qa_chain = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0.05), self.db.as_retriever(), memory=memory)
+        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True,
+                                            input_key='question', output_key='answer')
+        chatTemplate = """
+                        Answer the question based on the chat history(delimited by <hs></hs>) and context(delimited by <ctx> </ctx>) below. If you don't know the answer based on the given context, just say that you don't know, don't try to make up an answer. Keep the answers as concise as possible, but do explain your answer by quoting a piece of the context.
+                        -----------
+                        <ctx>
+                        {context}
+                        </ctx>
+                        -----------
+                        <hs>
+                        {chat_history}
+                        </hs>
+                        -----------
+                        Question: {question}
+                        Answer:
+                        """
+        templatePrompt = PromptTemplate(input_variables=["context", "question", "chat_history"], template=chatTemplate)
+        llm = self.setup_llm()
+        self.qa_chain = ConversationalRetrievalChain.from_llm(llm,
+                                                                self.db.as_retriever(),
+                                                                memory=memory,
+                                                                return_source_documents=self.save_convo,
+                                                                combine_docs_chain_kwargs={'prompt': templatePrompt} if local else None)
+
+
+    # Sets up the llm, depending on the use of the '--local' flag.
+    def setup_llm(self):
+        callbacks = []
+        if self.local:
+            if os.environ.get('MODEL_PATH') is None or not os.path.isfile(os.environ.get('MODEL_PATH')):
+                print("Please specify a valid model path that is to be used for local inference.")
+                raise Exception("No model specified")
+            return LlamaCpp(model_path=os.path.abspath(os.environ.get('MODEL_PATH')),
+                            max_tokens=8192,
+                            n_ctx=2048,
+                            n_gpu_layers=1,
+                            f16_kv = True,
+                            n_batch = 512,
+                            verbose=False, # Info about time taken to generate answer.
+                            callbacks=callbacks)
+
+        return OpenAI(temperature=0.05)
 
 
     def run(self):
         print("Starting chat. Type 'q' or 'exit' to quit.")
         while True:
             query = input(f"Model: {self.model}GPT. Ask your question: ")
-            if not query:
+            if not query or query.strip() == "":
                 continue
             if query == "q" or query == "exit":
+                self.finish_save_file()
                 return
             res = self.search(query)
-            print(res)
+            ans, src = res["answer"], [] if not self.save_convo else res['source_documents']
+            print(f"\t{ans}")
+            if self.save_convo:
+                self.save_conversation(query, ans, src)
             query = ""
+
+
+    # Saves the conversation by writing to a json file.
+    def save_conversation(self, query, answer, sources):
+        if self.save_path is None:
+            i = len([f for f in os.listdir("conversations/") if f.startswith(self.model)])
+            self.save_path = f"conversations/{self.model}-conversation{i:02d}.json"
+            with open(self.save_path, "a") as f:
+                conv = {"question": query, "answer": answer, "sources": [doc.page_content for doc in sources]}
+                f.write("[" + json.dumps(conv))
+            return
+
+        with open(self.save_path, "a") as f:
+            conv = {"question": query, "answer": answer, "sources": [doc.page_content for doc in sources]}
+            f.write("," + json.dumps(conv))
+
+
+    # Closes the json array.
+    def finish_save_file(self):
+        if self.save_path is None:
+            return
+        with open(self.save_path, "a") as f:
+            f.write("]")
 
 
     # Converts pdf file to txt file.
@@ -93,11 +177,16 @@ class UvaGPT:
 
     # Creates and saves Faiss db from the chunks generated by split_txt().
     def create_faiss_db(self, chunks):
-        store = FAISS.from_texts(chunks, OpenAIEmbeddings())
-        faiss.write_index(store.index, self.chunks_path)
-        store.index = None
+        if self.local:
+            print(f"Setting up local embedder using {'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'}")
+            db = FAISS.from_texts(chunks, HuggingFaceEmbeddings(model_name='sentence-transformers/multi-qa-MiniLM-L6-cos-v1',
+                                                                model_kwargs={'device': 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'})) # model_name="all-MiniLM-L6-v2"
+        else:
+            db = FAISS.from_texts(chunks, OpenAIEmbeddings())
+        faiss.write_index(db.index, self.chunks_path)
+        db.index = None
         with open(self.db_name, "wb") as f:
-            pickle.dump(store, f)
+            pickle.dump(db, f)
 
 
     # Loads Faiss db into memory.
@@ -113,12 +202,11 @@ class UvaGPT:
 
     # Searches for the query in the Faiss db.
     def search(self, query):
-        result = self.qa_chain({"question": query})
-        return result["answer"]
+        return self.qa_chain({"question": query})
 
 
 ALL_MODELS = get_all_models()
 ALL_MODELS.sort()
-model = parse_args()
-uva = UvaGPT(model=model)
+model, local, save = parse_args()
+uva = UvaGPT(local, save, model=model)
 uva.run()
